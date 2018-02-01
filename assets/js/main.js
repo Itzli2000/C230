d2c1f3bafd385d4e4a1b2c4113141caf9a47d2bf
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2018-02-01 14:44:00 -0600

    refactor: modify js render function
    to show complete information

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,6 @@
 var data;
+// Function to get data from json url file
+// and covert it to js object
 var txtFile = new XMLHttpRequest();
 txtFile.open("GET", "https://s3-us-west-2.amazonaws.com/lgoveabucket/data_melp.json", true);
 txtFile.onreadystatechange = function() {
@@ -16,7 +18,7 @@ txtFile.onreadystatechange = function() {
 }
 txtFile.send(null);
 
-
+// Function to show data object on frontend
 function render(){
   var html = data.map(function(message,index){
     return(`
@@ -25,6 +27,13 @@ function render(){
       <h3 class="card-title">${message.name}</h3>
       <p class="card-description">${message.contact.site}</p>
       <div class="card-price">${message.contact.email}</div>
+      <div class="card-price">${message.contact.phone}</div>
+      <div class="card-price">${message.address.street}</div>
+      <div class="card-price">${message.address.city}</div>
+      <div class="card-price">${message.address.state}</div>
+      <div class="card-price">${message.address.location.lat}</div>
+      <div class="card-price">${message.address.location.lng}</div>
+      <div class="card-price">${message.rating}</div>
       </div>
       `);
   }).join('  ');
