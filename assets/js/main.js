1ed83b3fcedc47051a5510ef61d7a906a986706f
Ariel Itzli Molina Sanchez <itzli2000@msn.com>
2018-02-01 16:37:46 -0600

    feature: modify nav bar styles and
    js functions

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,60 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block"; 
   setTimeout(showSlides, 7000);
 }
+
+// Active class for menu
+(function($) {
+  $("#mainNav li a").on("click", function(e) {
+    e.preventDefault();
+    $("#mainNav li a").removeClass("activemenu");
+    $(this).addClass("activemenu");
+  });
+  $("#logo").on("click", function(e) {
+    e.preventDefault();
+    $("#mainNav li a").removeClass("activemenu");
+  });
+})(jQuery);
+
+// Smooth scroll for menu anchors
+// Select all links with hashes
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+  // On-page links
+  if (
+    location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+    && 
+    location.hostname == this.hostname
+    ) {
+    // Figure out element to scroll to
+  var target = $(this.hash);
+  var navHeight = $('#mainNav').height();
+  var scrollToPosition = target.offset().top - (navHeight);
+  target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+    // Does a scroll target exist?
+    if (target.length) {
+      // Only prevent default if animation is actually gonna happen
+      event.preventDefault();
+      $('html, body').animate({
+        scrollTop: scrollToPosition
+      }, 1500, function() {
+        // Callback after animation
+        // Must change focus!
+        var $target = $(target);
+        $target.focus();
+      });
+    }
+  }
+});
+
+// Automatic close the navigation bar
+$('.navbar-collapse a').click(function(){
+  $(".navbar-collapse").collapse('hide');
+});
+
+
 // Function to get data from json url file
 // and covert it to js object
 var txtFile = new XMLHttpRequest();
